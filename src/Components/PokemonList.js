d4ac361f939fafe373a03549578c7ca371df04ac
Iqbal Fasri <iqbalx64@gmail.com>
2019-10-24 15:16:26 +0700

    status: styling pokemon card list

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -5,13 +5,22 @@ import { Link } from "react-router-dom";
 function PokemonList({ pokemons }) {
   return (
     <div className="container">
-      <ul>
-        {pokemons.map(pokemon => (
-          <li>
-            <Link to="/detail-pokemon">{pokemon.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="row">
+        {pokemons.map((pokemon, index) => {
+          return (
+            <div key={index} className="col-md-3">
+              <Link className="pokemon-card" to={`/detail/${pokemon.id}`}>
+                <div className="pokemon-card__image-wrapper">
+                  <img src={pokemon.image} />
+                </div>
+                <div className="pokemon-card__name">
+                  <h3>{pokemon.name}</h3>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
