ea3fcd888415ef18b7ec68229fd3711594f56662
Iqbal Fasri <iqbalx64@gmail.com>
2019-10-24 09:51:14 +0700

    Updates

---
+++ precache-manifest.8d8e25309e234136fabf9419846758dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e624c1970a989791c625d9b7e780d682",
+    "url": "/pokepi/index.html"
+  },
+  {
+    "revision": "efb2e1d5c827bd877489",
+    "url": "/pokepi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b7f09f7a6fd6ed26b6aa",
+    "url": "/pokepi/static/js/2.972ff825.chunk.js"
+  },
+  {
+    "revision": "efb2e1d5c827bd877489",
+    "url": "/pokepi/static/js/main.14f81a3c.chunk.js"
+  },
+  {
+    "revision": "d1e5e0b2f9bab682ac4a",
+    "url": "/pokepi/static/js/runtime-main.f3f0a615.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pokepi/static/media/logo.25bf045c.svg"
+  }
+]);
