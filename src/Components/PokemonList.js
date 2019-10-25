adabe0aa75207dd0f9da0ce1e1eee739cf53668b
Iqbal Fasri <iqbalx64@gmail.com>
2019-10-25 10:03:29 +0700

    status: detial pokemon with evolutions done

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -2,21 +2,21 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-function PokemonList({ pokemons }) {
+function PokemonList({ pokemons, disableLink }) {
   return (
     <div className="container">
       <div className="row">
         {pokemons.map((pokemon, index) => {
           return (
             <div key={index} className="col-md-3">
-              <Link className="pokemon-card" to={`/detail/${pokemon.id}`}>
+              <a className="pokemon-card" href={`/detail/${pokemon.id}`}>
                 <div className="pokemon-card__image-wrapper">
                   <img alt={pokemon.name} src={pokemon.image} />
                 </div>
                 <div className="pokemon-card__name">
                   <h3>{pokemon.name}</h3>
                 </div>
-              </Link>
+              </a>
             </div>
           );
         })}
--- src/Pages/DetailPokemon.js
+++ src/Pages/DetailPokemon.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import { fetchData } from "../lib";
 
 import LoadingModal from "../Components/LoadingModal";
+import PokemonList from "../Components/PokemonList";
 
 function DetailPokemon(props) {
   const [detailPokemon, setDetailPokemon] = useState(null);
@@ -17,6 +18,8 @@ function DetailPokemon(props) {
           image
           fleeRate
           maxCP
+          maxHP
+          weaknesses
 
           height {
             minimum
@@ -38,20 +41,98 @@ function DetailPokemon(props) {
 
       const { pokemon } = await fetchData(query);
       setDetailPokemon(pokemon);
-      setLoading(false)
+      setLoading(false);
     }
 
     getDetailPokemon();
-  });
+  }, [loading]);
 
   if (loading) {
     return <LoadingModal />;
   }
 
+  console.log(detailPokemon);
+
   return (
     <Fragment>
       <div className="container">
-        <h1>{detailPokemon.name}</h1>
+        <div className="pokemon-detail__wrapper">
+          <div className="row">
+            <div className="col-md-6 d-flex flex-column align-items-center">
+              <img alt={detailPokemon.name} src={detailPokemon.image} />
+              <div className="pokemon-detail__name">
+                <h1>{detailPokemon.name}</h1>
+              </div>
+            </div>
+            <div className="col-md-6 d-flex flex-column justify-content-center">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>Height</h3>
+                    <p>Minimum : {detailPokemon.height.minimum}</p>
+                    <p>Maximum : {detailPokemon.height.maximum}</p>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>Weight</h3>
+                    <p>Minimum : {detailPokemon.weight.minimum}</p>
+                    <p>Maximum : {detailPokemon.weight.maximum}</p>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>FleeRate</h3>
+                    <p>{detailPokemon.fleeRate}</p>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>MaxCP</h3>
+                    <p>{detailPokemon.maxCP}</p>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>MaxHP</h3>
+                    <p>{detailPokemon.maxHP}</p>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="pokemon-detail__info">
+                    <h3>Weakness</h3>
+                    <div className="d-flex flex-row">
+                      {detailPokemon.weaknesses.map((weakness, index) => (
+                        <p key={index}>{(index ? ", " : "") + weakness}</p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="pokemon-detail__wrapper">
+          <h1>Evolutions</h1>
+          <div className="pokemon-detail__wrapper-evolutions">
+            {detailPokemon.evolutions !== null ? (
+              <PokemonList
+                disableLink={true}
+                pokemons={detailPokemon.evolutions}
+              />
+            ) : (
+              <p>{"Evolutions not found."}</p>
+            )}
+          </div>
+        </div>
       </div>
     </Fragment>
   );
